8846aa2fc113e9540ea82567254d73d1b3a1f556
irdeen <calle_71@hotmail.com>
2016-11-07 17:21:25 +0100

    another imap test

--- server.js
+++ server.js
@@ -26,27 +26,29 @@ function openInbox(cb) {
 imap.once('ready', function() {
   openInbox(function(err, box) {
     if (err) throw err;
-    var f = imap.seq.fetch('1:3', {
-      bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-      struct: true
-    });
+    var f = imap.seq.fetch(box.messages.total + ':*', { bodies: ['HEADER.FIELDS (FROM)','TEXT'] });
     f.on('message', function(msg, seqno) {
       console.log('Message #%d', seqno);
       var prefix = '(#' + seqno + ') ';
       msg.on('body', function(stream, info) {
-        var buffer = '';
+        if (info.which === 'TEXT')
+          console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
+        var buffer = '', count = 0;
         stream.on('data', function(chunk) {
+          count += chunk.length;
           buffer += chunk.toString('utf8');
+          if (info.which === 'TEXT')
+            console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
         });
         stream.once('end', function() {
-          console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+          if (info.which !== 'TEXT')
+            console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+          else
+            console.log(prefix + 'Body [%s] Finished', inspect(info.which));
         });
       });
       msg.once('attributes', function(attrs) {
         console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-
-        //Here's were I imagine to need to do another fetch for the content of the message part...
-
       });
       msg.once('end', function() {
         console.log(prefix + 'Finished');
